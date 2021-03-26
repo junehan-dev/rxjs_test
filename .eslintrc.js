5a6b11c0acb99d2d0b057b9e68ac876e64fc9378
junehan <junehan.dev@gmail.com>
2021-03-26 19:01:49 +0900

    added plugins and linttest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,11 @@ module.export = {
     parser: '@typescript-eslint/parser',
     plugins: [
         '@typescript-eslint',
+        'eslint-plugin-import',
     ],
     extends: [
         'eslint:recommanded',
         'plugin:@typescript-eslint/recommanded',
+        'preetier',
     ],
 };
---
+++ srcs/main.ts
@@ -0,0 +1,1 @@
+let myStr: String = "myData";
